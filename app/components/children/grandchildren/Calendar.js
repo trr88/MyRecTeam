bea20c49832476177feff1d03a93d75b19952885
Kristen Bowen <kbowen200247@gmail.com>
2017-09-30 22:52:22 -0400

    Update Calendar.js

--- app/components/children/grandchildren/Calendar.js
+++ app/components/children/grandchildren/Calendar.js
@@ -2,7 +2,7 @@ var React = require("react");
 var Link = require("react-router").Link;
 var FullCalendar = require("fullcalendar");
 var jQuery = require("jquery");
-// var Modal = require("react-bootstrap-modal");
+var Modal = require("react-bootstrap-modal");
 var Moment = require("moment");
 
 var eventsArray = [ {
@@ -24,6 +24,7 @@ var eventsArray = [ {
         },{
             title: 'Practice Soccers',
             start: '2017-09-19T13:00:00+14:00',
+            end: '2017-09-19T14:00:00',
             description: 'This is a cool event'
 
         },{
@@ -40,16 +41,51 @@ var eventsArray = [ {
 
         }];
 
-var Calendar = React.createClass({
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
 
   render() {
+    let closeModal = () => this.setState({ open: false });
+
+    let saveAndClose = () => {
+      api.saveData().then(() => this.setState({ open: false }));
+    };
+
     return ( 
         <div>
-            <div id="calendar"></div>
+            <div id="calendar"></div>        
+            <Modal
+                show={this.state.open}
+                onHide={closeModal}
+                aria-labelledby="modalTitle"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="modalTitle">A Title Goes here</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="row">
+                        <div id="modalBody" className="col-md-9">
+                            <div className="modal-body"></div>
+                        </div>
+                        <div className="row">
+                            <div id="modalStart" className="col-md-4"></div>
+                            <div id="modalEnd" className="col-md-4"></div>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Modal.Dismiss className="btn btn-default">Close</Modal.Dismiss>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
-  },
+  }
+
   componentDidMount() {
+    var that = this;
 
     jQuery('#calendar').fullCalendar({
 			header: {
@@ -65,19 +101,17 @@ var Calendar = React.createClass({
             }
         },
         editable: false,
-		eventColor: '#303c6c'
-        // eventClick: function(event, jsEvent, view) {
-        //     // that.openModal();
-        //     code modal example from NPM till start
-        //  $('#modalTitle').html(event.title);
-        //  $('#modalBody').html(event.description);
-        //  $("#modalStart").html(moment(event.start).format('MMM Do h:mm A'));
-        //  $("#modalEnd").html(moment(event.end).format('MMM Do h:mm A'));
-        //  $('#fullCalModal').modal(); 
-        // }
+		eventColor: '#303c6c',
+        eventClick: function(event, jsEvent, view) {
+            that.setState({open:true});
+            jQuery('#modalTitle').html(event.title);
+            jQuery('#modalBody').html(event.description);
+            jQuery("#modalStart").html(moment(event.start).format('MMM Do h:mm A'));
+            jQuery("#modalEnd").html(moment(event.end).format('MMM Do h:mm A'));
+        }
     })
   }
-});
+};
 
 module.exports = Calendar;
 
